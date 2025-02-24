15ca8f08f9a96bf317d673e1aebfb851dd688c31
million-art <millionmulugeta09@gmail.com>
2025-02-24 21:12:23 +0300

    ui

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,7 +2,7 @@ import Profile from "../components/Profile"
 
 export const Home = () => {
   return (
-    <main className="flex flex-col h-screen mt-20 ">
+    <main className="flex flex-col h-screen mt-0 ">
        <Profile />
     </main>
   )
