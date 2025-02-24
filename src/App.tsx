ed4c5af06e51a1459d1a203df161f5d1c5ef08e4
million-art <millionmulugeta09@gmail.com>
2025-02-24 20:35:16 +0300

    update toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Provider } from "react-redux";
-import { store } from "./store/store"; 
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState, store } from "./store/store"; 
 import { ThemeProvider } from "next-themes"; 
 import { Routes, Route } from "react-router-dom";
 
@@ -11,12 +11,51 @@ import Order from "./screens/Order";
 import Products from "./screens/Products";
 import Cart from "./screens/Cart";
 import Referrals from "./screens/Referrals";
+import { toast, ToastContainer } from "react-toastify";
+import { useEffect } from "react";
+import { selectShowMessage, setShowMessage } from "./store/slice/messageReducer";
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const message = useSelector((state: RootState) => selectShowMessage(state));
+
+
+    // Show message as toast
+    useEffect(() => {
+      if (message) {
+        toast(message.message, {
+          autoClose: 2500,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          closeButton: false,
+        });
+        dispatch(setShowMessage(null));
+      }
+    }, [message, dispatch]);
+
   return (
-    <Provider store={store}>
       <ThemeProvider attribute="class" defaultTheme="system">
         <TopNav />
+        <ToastContainer
+        style={{
+          width: "calc(100% - 40px)",
+          maxWidth: "none",
+          left: "20px",
+          right: "20px",
+          top: "30px",
+          height: "80px",
+        }}
+        toastStyle={{
+          minHeight: "20px",
+          padding: "0px 10px",
+          paddingBottom: "4px",
+          backgroundColor: message?.color || "#00c000",
+          color: "white",
+          borderRadius: "6px",
+          marginBottom: "4px",
+        }}
+      />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/orders" element={<Order />} />
@@ -26,8 +65,7 @@ function App() {
         </Routes>
         <BottomNav />
       </ThemeProvider>
-    </Provider>
-  );
+   );
 }
 
 export default App;
